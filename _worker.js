f9e56b00187d11b8eb37171ad0c9bad3beb898af
wurabbie-daili <wurabbie@gmail.com>
2026-04-17 10:40:54 +0800

    Changed for zqi

--- _worker.js
+++ _worker.js
@@ -238,7 +238,7 @@ export default {
 						if (作为优选订阅生成器) ctx.waitUntil(请求日志记录(env, request, 访问IP, 'Get_Best_SUB', config_JSON, false));
 						else ctx.waitUntil(请求日志记录(env, request, 访问IP, 'Get_SUB', config_JSON));
 						const ua = UA.toLowerCase();
-						const expire = 1805385600;//2027-3-19 到期时间
+						const expire = 1794844800;//2026-11-17 到期时间
 						const nowSec = Math.floor(Date.now() / 1000);
 						const 剩余天数 = Math.max(0, Math.floor((expire - nowSec) / 86400));
 						const now = Date.now();
